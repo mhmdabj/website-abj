0453f455e3dad7492abe58bdeb78581dc06fdc0e
Mohamad Abuj <mohatabj@gmail.com>
2024-10-29 13:03:17 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: '#895197',
+        coral: '#FF6B6B',
+        green: '#99B84A',
+        blue: '#1E95E7',
+        orange: '#FDB65A',
+        black: '#1F1E25',
+        white: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
